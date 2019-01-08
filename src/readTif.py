49aea49cc79938b94d9f0335dea0ef20fc8d99c0
Xin Yao <susurrant@outlook.com>
2019-01-08 08:48:00 +0800

    Update readTif.py

--- src/readTif.py
+++ src/readTif.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 import tifffile
-'''
-img = cv2.imread('tif1.tif')
+
+img = cv2.imread('../data/VBZ1_201711251154_001_0046_L1A.tif')
 print(img.shape)
 
+'''
 v = set()
 
 for i in range(img.shape[0]):
@@ -12,14 +13,18 @@ for i in range(img.shape[0]):
         for k in range(3):
             v.add(img[i,j,k])
 print(v)
+'''
+tem = img[:,:,0]
+tem[np.where(tem==0)] = 255
 
-cv2.imwrite('gray.jpg', img)
+#cv2.imwrite('gray.jpg', img)
 cv2.namedWindow("Image")
-cv2.imshow("Image", img[500:1500,500:1500])
+cv2.imshow("Image", img[:,:])
 cv2.waitKey (0)
 cv2.destroyAllWindows()
-'''
 
-img = tifffile.imread('27566.tif')
+'''
+img = tifffile.imread('Raster_Airport.tif')
 print(img.shape, np.max(img))
-print(img[0,0])
+print(img[100])
+'''
