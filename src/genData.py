442f980eb90ebfcfac0a2c2cbf4704369d47bd6f
Xin Yao <susurrant@outlook.com>
2019-01-12 19:07:44 +0800

    Update genData.py

--- src/genData.py
+++ src/genData.py
@@ -49,8 +49,8 @@ def gen_data(obj_type, col_step, row_step, size):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '-type', help='object type', type=str)
-    parser.add_argument('-c', '--col_step', help='column step', const=100, type=int)
-    parser.add_argument('-r', '--row_step', help='row step', const=100, type=int)
-    parser.add_argument('-s', '--size', help='image size', const=1024, type=int)
+    parser.add_argument('-c', '-col_step', help='column step', const=100, type=int) #default: 100
+    parser.add_argument('-r', '-row_step', help='row step', type=int) #default: 100
+    parser.add_argument('-s', '-size', help='image size', type=int) #default: 1024
     args = parser.parse_args()
     gen_data(obj_types[2], args.col_step, args.row_step, args.size)
